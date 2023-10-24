10b18c6487c10de0aa19145ce968d4cb80c698fb
Bachir Sbinati <sbinatibachir@gmail.com>
2023-10-24 12:51:21 +0200

    Refactor register endpoint logic

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,4 +1,19 @@
 import Services from '@/services';
+import Utils from '@/utils';
+
+export async function createUser(username, password) {
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Item: {
+      PK: username,
+      SK: username,
+      password: Utils.bcrypt.hashPassword(password),
+    },
+    ConditionExpression: 'attribute_not_exists(PK)',
+  };
+
+  await Services.db.put(params).promise();
+}
 
 export async function getUser(username) {
   const params = {
