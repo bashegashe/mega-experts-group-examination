0b7a12f7c373fa64bbefed6f75f7fd52211d04fd
joakimtrulsson <joakim.trulsson@icloud.com>
2023-10-24 09:28:43 +0200

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mega-experts-group-examination/',
+  // base: '/mega-experts-group-examination/',
 });
