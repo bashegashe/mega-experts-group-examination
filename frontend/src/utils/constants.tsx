da03a92c44fe65f246e6f7287293f5495cabefac
Bachir Sbinati <sbinatibachir@gmail.com>
2023-10-25 12:43:23 +0200

    Test fix for github pages api url issue

--- frontend/src/utils/constants.tsx
+++ frontend/src/utils/constants.tsx
@@ -1,10 +1,13 @@
 export const BASE_URI = 'https://bashegashe.github.io/mega-experts-group-examination/';
 // export const API_URI = 'https://1z1eshubtb.execute-api.eu-north-1.amazonaws.com/api/';
 
+let cachedAPIURI: string | null = null;
 const getAPIURI = () => {
+  if (cachedAPIURI) return cachedAPIURI;
   const search = window.location.search;
   const params = new URLSearchParams(search);
   const api_uri = import.meta.env.VITE_APP_API_URL || params.get('api_url');
+  cachedAPIURI = api_uri;
   return api_uri
 } 
 
