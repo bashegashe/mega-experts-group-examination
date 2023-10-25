f6be1a0e271de30dbdf36eacd363a78986c629dd
Bachir Sbinati <sbinatibachir@gmail.com>
2023-10-25 11:14:40 +0200

    Set date for random meetups to one day from now so they become visible when fetching meetups

--- backend/scripts/addRandomMeetups.js
+++ backend/scripts/addRandomMeetups.js
@@ -9,12 +9,15 @@ const generateMeetup = () => {
   const PK = `MEETUP#${id}`;
   const SK = `MEETUP#${id}`;
 
+  const currentDate = new Date();
+  currentDate.setDate(currentDate.getDate() + 1);
+
   return {
     PK,
     SK,
     title: `Träff i ${['Stockholm', 'Göteborg', 'Malmö'][Math.floor(Math.random() * 3)]}`,
     host: `${['Erik', 'Anna', 'Lars', 'Karin'][Math.floor(Math.random() * 4)]} Svensson`,
-    date: new Date().toISOString(),
+    date: currentDate.toISOString(),
     location: `${['Vasaparken', 'Slottsskogen', 'Ribersborgsstranden'][Math.floor(Math.random() * 3)]}`,
     description: 'En trevlig träff för att umgås och njuta av god mat och sällskap.',
     rating: Math.floor(Math.random() * 5) + 1,
