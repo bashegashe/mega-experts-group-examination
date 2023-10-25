189dd1e3182a2a05773844bb86864409c1879b30
Bachir Sbinati <sbinatibachir@gmail.com>
2023-10-25 12:36:12 +0200

    Test api url search param for github pages

--- frontend/src/utils/constants.tsx
+++ frontend/src/utils/constants.tsx
@@ -1,3 +1,11 @@
 export const BASE_URI = 'https://bashegashe.github.io/mega-experts-group-examination/';
 // export const API_URI = 'https://1z1eshubtb.execute-api.eu-north-1.amazonaws.com/api/';
-export const API_URI = import.meta.env.VITE_APP_API_URL;
+
+const getAPIURI = () => {
+  const search = window.location.search;
+  const params = new URLSearchParams(search);
+  const api_uri = import.meta.env.VITE_APP_API_URL || params.get('api_url');
+  return api_uri
+} 
+
+export const API_URI = getAPIURI();
