25e6c7a8eeda8883dae9447121ff470a99023a02
Pile-o-pancakes <112945412+Pile-o-pancakes@users.noreply.github.com>
2023-10-27 12:15:37 +0200

    0.1

--- frontend/src/pages/Meetups.tsx
+++ frontend/src/pages/Meetups.tsx
@@ -1,62 +1,12 @@
+
 import Menu from '../components/Menu/Menu';
 
 function Meetups() {
-  const APIURI = import.meta.env.VITE_APP_API_URL;
-
-  const fetchProfile = async () => {
-    try {
-      const url = `${APIURI}/profile`;
-      const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        credentials: 'include',
-      });
-
-      if (response.ok) {
-        const responseData = await response.json();
-        console.log(responseData);
-      } else {
-        alert('Något gick fel vid hämtning av profil.');
-        console.error('Hämtning av profil misslyckades.');
-      }
-    } catch (error) {
-      console.error('Ett fel uppstod:', error);
-    }
-  };
-
-  const fetchMeetups = async () => {
-    try {
-      const url = `${APIURI}/meetups`;
-      const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        credentials: 'include',
-      });
-
-      if (response.ok) {
-        const responseData = await response.json();
-        console.log(responseData);
-      } else {
-        alert('Något gick fel vid hämtning av meetups.');
-        console.error('Hämtning av meetups misslyckades.');
-      }
-    } catch (error) {
-      console.error('Ett fel uppstod:', error);
-    }
-  }
 
   return (
     <main className='main'>
       <h3 className='main__title'>Meetups</h3>
       <Menu />
-      <div style={{marginBottom: '1rem'}}>
-        <button onClick={fetchProfile}>Fetch profile</button>
-      </div>
-      <button onClick={fetchMeetups}>Fetch meetups</button>
     </main>
   );
 }
