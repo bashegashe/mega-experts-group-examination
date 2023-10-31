26e6f34f050ae3783ab15bb5cd55d4c42be9a8f1
joakimtrulsson <joakim.trulsson@icloud.com>
2023-10-31 11:16:19 +0100

    See descripton.
    Fixed tsc errors.
    Removed Menu from Login.
    Added a success message with a countdown in the Signup component to inform users of a successful registration.

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,18 +1,16 @@
+import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-
 import Logo from '../components/Logo/Header';
 import AuthForm from '../components/AuthForm/AuthForm';
-
 import { postSignup } from '../services/api';
 import { FormData } from '../types/types';
 import Loader from '../components/Loader/Loader';
 
-// const APIURI = import.meta.env.VITE_APP_API_URL;
-
 function Signup() {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
+  const [registrationSuccess, setRegistrationSuccess] = useState(false);
+  const [countdown, setCountdown] = useState(3);
 
   const handleSignup = async (formData: FormData) => {
     if (!formData.username || !formData.password) return;
@@ -27,12 +25,13 @@ function Signup() {
       const response = await postSignup(JSON.stringify(requestBody));
 
       if (response.success) {
-        // setIsLoading(false);
-        navigate('/login');
+        setRegistrationSuccess(true);
       } else if (response.error === 'password length must be at least 6 characters long') {
         alert('Lösenordet måste innehålla minst 6 tecken.');
+      } else if (response.error === 'Username already exists') {
+        alert('Det här användarnamnet är upptaget.');
       } else {
-        alert('Något gick fel vid registeringen.');
+        alert('Något gick fel vid registreringen.');
         console.error('Registreringen misslyckades.', response);
       }
     } catch (error) {
@@ -42,12 +41,34 @@ function Signup() {
       setIsLoading(false);
     }
   };
+
+  useEffect(() => {
+    if (registrationSuccess) {
+      const countdownTimer = setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1);
+      }, 1000);
+
+      if (countdown === 0) {
+        navigate('/login');
+      }
+
+      return () => {
+        clearInterval(countdownTimer);
+      };
+    }
+  }, [registrationSuccess, countdown, navigate]);
+
   return (
     <main className='main'>
       <Logo />
       <h3 className='main__title'>Skapa en användare</h3>
       <AuthForm onSubmit={handleSignup} initialValues={{ username: '', password: '' }} />
       {isLoading && <Loader />}
+      {registrationSuccess && (
+        <p className='main__success'>
+          Registreringen lyckades, du skickas vidare till inloggning om {countdown}...
+        </p>
+      )}
       <p className='auth__changeview'>
         Har du redan ett konto? Logga in <Link to='/login'>här</Link>
       </p>
